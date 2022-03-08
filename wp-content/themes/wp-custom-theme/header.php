45a8a0318ceb04de4aee2ca71f08269234d6e6d0
[cans1994] <cans.bl@hotmail.com>
2022-03-09 00:44:24 +0100

    reglage

--- wp-content/themes/wp-custom-theme/header.php
+++ wp-content/themes/wp-custom-theme/header.php
@@ -21,20 +21,21 @@
     <?php wp_head(); ?>
 </head>
 
-<body <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Menu</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Menu</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <?php wp_nav_menu([
-                'theme_location' => 'header',
-                'container' => false,
-                'menu_class' => 'navbar-nav me-auto',
-            ]) ?>
-            <!--<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <?php wp_nav_menu([
+                    'theme_location' => 'header',
+                    'container' => false,
+                    'menu_class' => 'navbar-nav me-auto',
+                ]) ?>
+                <!--<ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
@@ -60,9 +61,9 @@
             </ul>
             -->
 
+            </div>
         </div>
-    </div>
-    <?= get_search_form() ?>
+        <?= get_search_form() ?>
 
     </nav>
 
